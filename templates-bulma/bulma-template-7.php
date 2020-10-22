a32b3e45356c5d32a88457df9a490da9376aee6c
Chance Corbeil <corbeilchance@gmail.com>
2020-10-21 17:46:59 -0700

    Add custom sass colors as mixins

--- templates-bulma/bulma-template-7.php
+++ templates-bulma/bulma-template-7.php
@@ -55,3 +55,67 @@
 
 </section>
 <!-- End: blog loop for posts -->
+
+
+
+
+
+
+
+<!-- Begin: blog loop for posts -->
+<section class="section pageparticlecontainer hero is-primary" id="bulmatemplate7">
+
+  <div class="container pageparticle">
+    <div class="columns">
+      <div class="column is-12">
+        <h2 class="title is-2">Bulma Template 7 - Part B</h2>
+        <p  class="subtitle">Blop loop - 3 cards</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container pageparticle">
+    <div class="columns card has-text-centered">
+
+      <?php wp_reset_query(); ?>
+
+      <?php
+  $args = array(
+    'posts_per_page' => 3,
+    'post_type' => 'post',
+    'orderby' => 'date',
+    'order' => 'DESC',
+  );
+  $loop = new WP_Query( $args ); 
+  ?>
+
+      <?php while ( $loop->have_posts() ) : $loop->the_post();?>
+
+      <div class=" column is-4">
+        <div class="card-image">
+          <figure class="image">
+            <?php echo get_the_post_thumbnail(); ?>
+          </figure>
+        </div>
+        <div class="card-content">
+
+          <div class="content">
+            <h2 class="title is-4"><a href="<?php echo get_the_permalink(); ?>"><?php echo get_the_title(); ?></a></h2>
+            
+                                <!-- Subtitle -->
+                                <?php if( get_field('subtitle') ): ?>
+                                  <p class="subtitle"><?php the_field('subtitle'); ?></p>
+                    <?php endif; ?>
+
+          </div>
+        </div>
+      </div>
+
+      <?php endwhile; ?>
+      <?php wp_reset_query(); ?>
+
+    </div>
+  </div>
+
+</section>
+<!-- End: blog loop for posts -->
---
+++ templates-bulma/bulma-template-8.php
@@ -0,0 +1,58 @@
+<!-- Section 8 : Color Demo -->
+<section class="section pageparticlecontainer" id="bulmatemplate8">
+
+  <div class="container pageparticle">
+    <div class="columns">
+      <div class="column is-12">
+        <h2 class="title is-2">Bulma Template 8</h2>
+        <p  class="subtitle">The purpose of this section is to explore color variables is SASS</p>
+      </div>
+    </div>
+  </div>
+
+  <div class="container pageparticle">
+    <div class="columns">
+      <div class="column is-12">
+        <button class="button is-primary">Primary</button>
+        <button class="button is-2 is-secondary">Secondary</button>
+        <button class="button is-2 is-accent">Accent</button>
+      </div>
+    </div>
+  </div>
+
+  <div class="container pageparticle">
+    <div class="columns">
+      <div class="column is-4">
+        <h2 class="title has-text-primary">Primary</h2>
+      </div>
+      <div class="column is-4">
+        <h2 class="title has-text-secondary">Secondary</h2>
+      </div>
+      <div class="column is-4">
+        <h2 class="title has-text-accent">Accent</h2>
+      </div>
+    </div>
+  </div>
+
+  <div class="container pageparticle">
+    <div class="columns">
+      <div class="column is-4">
+      <div class="box has-background-primary">Primary</div>
+        <div class="box has-background-primary-light">Primary Light</div>
+        <div class="box has-background-primary-dark">Primary Dark</div>
+      </div>
+      <div class="column is-4">
+        <div class="box has-background-secondary">Secondary</div>
+        <div class="box has-background-secondary-light">Secondary Light</div>
+        <div class="box has-background-secondary-dark">Secondary Dark</div>
+      </div>
+      <div class="column is-4">
+        <div class="box has-background-accent">Accent</div>
+        <div class="box has-background-accent-light">Accent Light</div>
+        <div class="box has-background-accent-dark">Accent Dark</div>
+      </div>
+    </div>
+  </div>
+
+</section>
+<!-- Section 8 : Color Demo -->
