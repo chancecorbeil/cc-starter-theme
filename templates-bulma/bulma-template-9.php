008b92c85a2202992fc64108955fffad4653a81f
Chance Corbeil <corbeilchance@gmail.com>
2020-10-28 02:34:07 -0700

    Clean up styles and files

--- templates-bulma/bulma-template-9.php
+++ templates-bulma/bulma-template-9.php
@@ -1,15 +1,116 @@
 <!-- Section 9 -->
 <section class="section pageparticlecontainer" id="bulmatemplate9">
 
-  <div class="container pageparticle">
+<div class="container">
     <div class="columns">
       <div class="column is-12">
         <h2 class="title is-2">Bulma Template 9</h2>
-        <p  class="subtitle">Coming soon yah filthy animal</p>
+        <p  class="subtitle">Blah blah blah</p>
       </div>
     </div>
   </div>
 
+<div class="container">
+
+    <div class="columns has-text-centered">
+            <div class="column">
+                <h2 class="title is-2 has-text-secondary has-text-purple1lightest">Happy Client Testimonials</h2>
+            </div>
+        </div>
+
+        <div class="columns testimonial">
+            <div class="column is-3">
+                <div class="image"><img src="<?php echo get_template_directory_uri() ?>/images/avatar1.svg" alt="" width="300"
+                    class="starlogo" /></div>
+            </div>
+            <div class="column is-9">
+            <div class="box">
+                <div class="testimonialquote is-size-4 is-family-secondary"><span
+                        class="quotemarks">"</span>This morning both Susan and Barb spontaneously told me that they think you're the greatest. Apparently you're the first tech guy they've worked with who explains things in an understandable way! 😊  Well done.<span class="quotemarks">"</span></div>
+            </div>
+            </div>
+        </div>
+
+        <div class="columns testimonial">
+            <div class="column is-9">
+            <div class="box">
+                <div class="testimonialquote is-size-4 is-family-secondary"><span
+                        class="quotemarks">"</span>Chance- you're awesome!! I know I tell you all the time but YOU DA MAN!!! I'm feeling so much better about our site since you came on board.<span class="quotemarks">"</span></div>
+            </div>
+            </div>
+            <div class="column is-3">
+            <div class="image"><img src="<?php echo get_template_directory_uri() ?>/images/avatar4.svg" alt="" width="300"
+                    class="starlogo" /></div>
+            </div>
+        </div>
+
+    </div>
+
+    <div class="container">
+    <div class="columns">
+      <div class="column is-4">
+      <div class="dropdown">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+      <span>Content</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+    <div class="dropdown-content">
+      <div class="dropdown-item">
+        <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+      </div>
+      <hr class="dropdown-divider">
+      <div class="dropdown-item">
+        <p>You simply need to use a <code>&lt;div&gt;</code> instead.</p>
+      </div>
+      <hr class="dropdown-divider">
+      <a href="#" class="dropdown-item">
+        This is a link
+      </a>
+    </div>
+  </div>
+</div>
+      </div>
+      <div class="column is-4">
+      <div class="dropdown is-active">
+  <div class="dropdown-trigger">
+    <button class="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+      <span>Content</span>
+      <span class="icon is-small">
+        <i class="fas fa-angle-down" aria-hidden="true"></i>
+      </span>
+    </button>
+  </div>
+  <div class="dropdown-menu" id="dropdown-menu2" role="menu">
+    <div class="dropdown-content">
+      <div class="dropdown-item">
+        <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+      </div>
+      <hr class="dropdown-divider">
+      <div class="dropdown-item">
+        <p>You simply need to use a <code>&lt;div&gt;</code> instead.</p>
+      </div>
+      <hr class="dropdown-divider">
+      <a href="#" class="dropdown-item">
+        This is a link
+      </a>
+    </div>
+  </div>
+</div>
+      </div>
+      <div class="column is-4">
+        <h2 class="title is-2">Bulma Templatdd</h2>
+      </div>
+    </div>
+  </div>
 
 </section>
 <!-- Section 9 -->
+
+
+
+
